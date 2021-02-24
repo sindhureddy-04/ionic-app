13a5eda3718e794b89f2fdabffc1f2144dfe4ea5
Sai <Nithyam1330>
2021-02-24 20:39:03 +0530

    first

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+        
+    
+  
+
+
+
